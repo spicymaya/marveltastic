d65f651d8ad9a359f36c2bc0d4f7439b105a610d
Maya Davis <kryvitskaya@gmail.com>
2020-09-06 15:12:41 -0400

    wrapper component

---
+++ src/MarvelContainer/MarvelContainer.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import md5 from 'md5';
+// import { debounce } from '../helpers/renderHelpers';
+import useDebouncedSearch from '../helpers/useDebouncedSearch';
+import MarvelCharacter from '../MarvelCharacter/MarvelCharacter';
+import Header from '../Header/Header';
+import styles from './MarvelContainer.module.css';
+
+const ts = Date.now();
+const publicKey = '23a1b44bdda4bdc76b8cd4e4a125b5da';
+const privateKey = '0778a2817a560328adbad44309187a1b837ccd0e';
+const hash = md5(ts + privateKey + publicKey);
+const initialCharactersUrl = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=12`;
+const getSingleCharactersUrl = (name) =>
+  `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&name=${name}`;
+
+const getAllCharacters = async () => {
+  const response = await fetch(initialCharactersUrl, {
+    method: 'GET',
+  });
+  return response.json();
+};
+const getSingleCharacter = async (name) => {
+  const response = await fetch(getSingleCharactersUrl(name), {
+    method: 'GET',
+  });
+  return response.json();
+};
+const useGetSingleCharacter = () => useDebouncedSearch((name) => getSingleCharacter(name), 500);
+
+const MarvelContainer = () => {
+  const [initCharacters, setInitCharacters] = useState();
+  const { inputText, setInputText, searchResults } = useGetSingleCharacter();
+  const [showResults, setShowResults] = useState(false);
+
+  useEffect(async () => {
+    const data = await getAllCharacters();
+    setInitCharacters(data.data.results);
+  }, []);
+
+  useEffect(() => {
+    if (
+      searchResults &&
+      searchResults.result &&
+      searchResults.result.data &&
+      searchResults.result.data.results.length &&
+      inputText
+    ) {
+      setShowResults(true);
+    } else {
+      setShowResults(false);
+    }
+  }, [searchResults, inputText]);
+
+  return (
+    <>
+      <Header />
+      <div className={styles.grid}>
+        <div className={styles.inputContianer}>
+          <input
+            className={styles.input}
+            placeholder="Type full character name to search"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+        </div>
+
+        {!showResults
+          ? initCharacters && (
+              <div className={styles.row}>
+                {initCharacters.map((character) => (
+                  <MarvelCharacter name={character.name} thumbnail={character.thumbnail} />
+                ))}
+              </div>
+            )
+          : searchResults.result.data.results && (
+              <div className={styles.row}>
+                {searchResults.result.data.results.map((character) => (
+                  <MarvelCharacter name={character.name} thumbnail={character.thumbnail} />
+                ))}
+              </div>
+            )}
+      </div>
+    </>
+  );
+};
+export default MarvelContainer;
