db24b28892e4029be8d9fefb9536ed03480e5708
Maya Davis <kryvitskaya@gmail.com>
2020-09-06 15:13:17 -0400

    header component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => (
+  <div className={styles.header}>
+    <h1 className={styles.title}>Welcome to Marveltastic!</h1>
+  </div>
+);
+export default Header;
