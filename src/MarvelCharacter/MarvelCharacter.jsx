7ecb632e53cd6e2be4d1df43a3551e11ccf18663
Maya Davis <kryvitskaya@gmail.com>
2020-09-07 11:42:24 -0400

    add test id

--- src/MarvelCharacter/MarvelCharacter.jsx
+++ src/MarvelCharacter/MarvelCharacter.jsx
@@ -5,8 +5,15 @@ const MarvelCharacter = ({ name, thumbnail }) => {
   const { path, extension } = thumbnail;
   return (
     <div className={styles.characterCard}>
-      <img src={`${path}.${extension}`} alt={name} className={styles.responsiveImg} />
-      <div className={styles.name}>{name}</div>
+      <img
+        src={`${path}.${extension}`}
+        alt={name}
+        className={styles.responsiveImg}
+        data-testid="characterImg"
+      />
+      <div className={styles.name} data-testid="characterName">
+        {name}
+      </div>
     </div>
   );
 };
